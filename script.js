f7b099cd31127c84fcf83428796ed42881b33c0a
joaolipinho3-oss <joaolipinho3@gmail.com>
2026-06-11 15:08:51 -0300

    modified:   script.js 	modified:   styles.css

--- script.js
+++ script.js
@@ -2,13 +2,19 @@ const botaoSoma = document.getElementById('soma'); // Botão de +
 const botaoSubtracao = document.getElementById('subtracao'); // Botão de menos
 const increment = document.getElementById('changeBy'); // Mudar incremento
 const tela = document.getElementById('display-counter');
+const reset = document.getElementById('reset-button');
 
 console.log(botaoSoma);
 console.log(botaoSubtracao);
 console.log(increment);
 console.log(tela);
+console.log(reset)
 
 let contador = 0;
+if (contador < 0) {
+    alert('O número mínimo foi atingido 0')
+    contador = 0
+}
 
 function atualizarTela() {
   tela.textContent = contador;
@@ -18,14 +24,20 @@ botaoSoma.addEventListener('click',() => {
     console.log("Clique no +");
     contador += parseInt(increment.value);
     atualizarTela();
-    console.log(`Contador: ${contador}` )
+    console.log(`Contador: ${contador}`)
 });
 
 botaoSubtracao.addEventListener('click', () => {
     console.log("Clique no -");
     contador -= parseInt(increment.value);
     atualizarTela();
-    console.log(`Contador: ${contador}` )
+    console.log(`Contador: ${contador}`)
 });
+reset.addEventListener('click', () => {
+    console.log("Reset pressionado")
+    contador = 0
+    atualizarTela()
+    console.log(`Contador: ${contador}`)
+})
 
 botaoSoma.addEventListener('keydown', )
