682c82d82aa6830c524f8e1e1bf0b84b4d3a113b
joaolipinho3-oss <joaolipinho3@gmail.com>
2026-06-11 10:52:47 -0300

    new file:   Pasta CSS/styles.css 	modified:   index.html 	modified:   script.js 	modified:   styles.css

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-const buttonPlus = document.getElementById('soma') // Botão de +
-const buttonMinus = document.getElementById('subtracao') // Botão de menos
+const botaoSoma = document.getElementById('soma'); // Botão de +
+const botaoSubtracao = document.getElementById('subtracao'); // Botão de menos
+const increment = document.getElementById('changeBy'); // Mudar incremento
+const tela = document.getElementById('display-counter');
 
-const tela = document.getElementById('display-counter') // Tela do contador
+let contador = 0;
 
-const increment = document.getElementById('changeBy') // Mudar incremento
+function atualizarTela() {
+  tela.textContent = contador;
+};
 
-buttonMinus.addEventListener('click')
+botaoSoma.addEventListener('click', () => {
+    contador += parseInt(increment.value);
+    atualizarTela();
+});
+
+botaoSubtracao.addEventListener('click', () => {
+    contador -= parseInt(increment.value);
+    atualizarTela();
+});
